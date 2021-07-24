5a513a2c394f6b755845416337e9a2bd2d728cf2
hwheeless1 <75491936+hwheeless1@users.noreply.github.com>
2021-07-24 12:26:11 -0400

    Update pcap-parser.py

--- pcap-parser.py
+++ pcap-parser.py
@@ -12,33 +12,22 @@ from pyfiglet import Figlet
 # Start building out text for user functionality
 # Print out the enviroment
 
-def banner_message():
-    f = Figlet(font='slant')
-    print (f.renderText("PCAParser"))
+def banner_message(message):
+    if message == "start":
+        f = Figlet(font='slant')
+        return (f.renderText("PCAParser"))
 
-def live_capture():
-    os.system("tshark -D")
+def live_capture(param):
+    return(os.system("tshark -T fields -e frame.time -i {} -w {}.pcap > {} -F pcap -c {}".format(*param)))
 
-    cap_int = str(input("Which interface are you looking to scan?"))
-    out_file = str(input("Where do you want this file saved?"))
-    # packet_check = str(input("Do you want a limit on packet amount?[y/n]"))
-    # if packet_check == "y" or "yes" or "Y" or "Yes":
-    packet_count = int(input("How many packets do you want counted?"))
-    # else:
-    #    pass  
-    os.system("tshark -T fields -e frame.time -i {} -w {}.pcap > {} -F pcap -c {}".format(cap_int, out_file, out_file, packet_count))
+def convert(param2):
+    os.system ('tshark -r {} > {}.txt'.format(*param2))
 
-def convert():
-    filename = input("Enter your filename:")
-    x = str(input("Enter your destination file here:"))
-    os.system ('tshark -r'+filename +'>'+ x +'.txt')
-
-def counters():
+def counters(filename):
     counter=0
     ipcounter=0
     tcpcounter=0
     udpcounter=0
-    filename = input("Enter your filename:")
     if filename[-2:] == "ng":
         x = dpkt.pcapng.Reader(open(filename, 'rb'))
     else:
@@ -59,22 +48,75 @@ def counters():
 
         if ip.p==dpkt.ip.IP_PROTO_UDP:
             udpcounter+=1
-    print ("Beginning PCAP Parser")
+    print ("Rough amount of data in your current PCAP:")
     print ("Total number of packets in the pcap file: ", counter)   
     print ("Total number of ip packets: ", ipcounter)
     print ("Total number of tcp packets: ", tcpcounter)
     print ("Total number of udp packets: ", udpcounter)
 
-def export():
-    filename = input("Enter your filename: ")
-    dst = input("Enter destination file :")
-    print("What file type are you looking for? Enter only 1\n dicom \n http \n imf \n smb \n tftp")
-    export = input("Enter export object type: ")
-    os.system("tshark -r  {} --export-objects {}, {}" .format  (filename, export, dst))
+def export(param4):
+        os.system("tshark -r  {} --export-objects {}, {}" .format  (*param4))
 
+def input_check(usr_prompt, error, is_valid, valid_list):
+    user_input = input(usr_prompt)
+    while not is_valid(user_input, valid_list):
+        print(error)
+        user_input = input(usr_prompt)
+    return user_input
+
+# input_check helper function.
+def validate_list(item, lst):
+    if item not in lst:
+        return False
+    return True
+ 
+# input_check helper function.
+def validate_number(item, lst):
+    if item.isdigit() and int(item) <= len(lst):
+        return True
+    return False
+ 
 def main():
-    if 'a' == 'a':
-        print("Hello")
+    print(banner_message("start"))
+    # print(WELCOME MESSAGE)
+    print("Are you working on a live capture or existing PCAP?")
+    choice = input_check("Type L for live Capture OR E for Existing PCAP   ", "Invalid input. Expected L or M." , validate_list, ["E", "e", "L", "l"])
+    if choice in ["L", "l"]:
+        os.system("tshark -D")
+        cap_int = str(input("Which interface are you looking to scan?"))
+        out_file1 = str(input("Where do you want this file saved?"))
+        out_file2 = "/tmp/"+out_file1
+        print("Your file will be saved as " + out_file2 + " and as "+out_file2+".pcap")
+        packet_count = int(input("How many packets do you want counted?"))
+        param = cap_int, out_file2, out_file2, packet_count
+        print(live_capture(param))
+    
+    elif choice in ["E", "e"]:
+        os.system("ls -hl *pcap*")
+        filename = input("Enter your filename: ")
+        print(counters(filename))
+        print("Are you looking to convert this PCAP to a CSV?")
+        choice2 = input_check("Y or N?   ", "Invalid input, Expected Y or N." , validate_list, ["yes", "Yes", "Y", "y", "No", "no", "n", "N"])
+        if choice2 in ["yes", "Yes", "Y", "y"]:
+            dst_file = str(input("Enter your destination file here:"))
+            dst_file2 = "/tmp/" + dst_file
+            print("Your file will be saved as" + dst_file2)
+            param2 = filename, dst_file2
+            print(convert(param2))
+        elif choice2 in ["No", "no", "n", "N"]:
+            print("So are we looking to Export[X] or Search[S] within the PCAP?")
+            choice3 = input_check("S or X  ", "Invalid input, Expected S or X." , validate_list ["S", "s", "X", "x"])
+            if choice3 in ["x", "X"]:
+                dst_file = str(input("Enter your destination file here:"))
+                dst_file2 = "/tmp/"+dst_file
+                print("Your file will be saved as" + dst_file2)
+                print("What file type are you looking for? Enter only 1:\n dicom \n http \n imf \n smb \n tftp")
+                export = input("Enter export object type: ")
+                param4 = filename, export, dst_file2
+                print(export(param4))
+            # elif choice3 in 
+
+    
 
 if __name__ == '__main__':      
     main()        
