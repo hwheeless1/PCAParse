bb2d6bf89d216ee238cb64f7e5f2221c9244d279
xDRE21 <78388935+xDRE21@users.noreply.github.com>
2021-07-24 12:58:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,6 @@ ln -s "$(pwd)"/PCAParse.py /usr/bin/PCAParse
 apt-get install python3-pip
 pip install dpkt
 pip install scapy
+pip install pyfiglet
 echo
 echo 'You Have Installed PCAParse and All Required Packages!'
