a5d388c93bfbb8bdfb08dcf4b33aa313737a4fcb
xDRE21 <78388935+xDRE21@users.noreply.github.com>
2021-07-24 11:26:09 -0400

    Update setup.sh
    Simple bash script to download our program and it's modules

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+#Creating a Setup Folder wherever you run this script from
+#Download the code for our program from GitHub
+#Make it executable as well as a command itself
+
 mkdir -p ./Setup
 cd Setup
 wget -O PCAParse.py https://raw.githubusercontent.com/hwheeless1/PCAParse/main/pcap-parser.py 
 chmod +x PCAParse.py
-scriptdir=`dirname "$BASH_SOURCE"`
 ln -s "$(pwd)"/PCAParse.py /usr/bin/PCAParse
+
+#Download Required Modules for Python Script
+
 apt-get install python3-pip
 pip install dpkt
 pip install scapy
